161c2afa11399622f82f8f639665115e415c0dc3
hrshit7 <harshitchoudhary000012@gmail.com>
2024-10-17 15:36:11 +0530

    http server, url, http methods

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# This is a repository Where i put my code of learning Node.js
+
+# Till now 
+- Modules 
+    - fs, http
+- Nodejs working 
+- Build http server
+- URL 
+- Http Methods 
+    - GET
+    - POST
+    - PUT 
+    - PATCH
+    - DELETE
+- Express    
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const http = require("http");
-const fs = require("fs");
+const http = require("http");    //http modeule
+const fs = require("fs");        // file system module
+const url = require("url");      // url library for specify url information
 
-const myServer = http.createServer((req, res)=>{
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
-    fs.appendFile("logged.txt", log, (err,data)=>{
-        switch (req.url) {
+const myServer = http.createServer((req, res)=>{                       // create server is used to create a server
+    if(req.url == '/favicon.ico') return res.end();
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;         // in this log we are creating a log file a store information of app pages
+    const myUrl = url.parse(req.url , true);                            //use of url library just to parse a url passes two parameter one is url, second is the query parse parameter and also there are many parameter we can use
+    console.log(myUrl);
+    
+    fs.appendFile("logged.txt", log, (err,data)=>{                      //created and append a file on every page reload
+        switch (myUrl.pathname) {
             case "/":
-                res.end("HomePage");
+                if(req.url === 'GET') return res.end("HomePage");      //get request is to get data from server
                 break;
             case "/about":
-                res.end("About Us");
+                const lastName = myUrl.query.ln;
+                res.end("last name is " + lastName);
                 break;
+            case "/search":
+                const searchQuery = myUrl.query.search;
+                res.end("Search Results of your Query " + searchQuery);
+                break;    
+            case "/signup":
+                if(req.url === 'GET') return res.end("This is a signup form");         //here we take data from user 
+                else if(req.url === 'POST'){                                           // and from here we simply collect the data to server and store in a db and returns success to user
+                    // db.store
+                    return res.end("Success");
+                }
+                break;     
             default:
                 res.end("404");
         }
